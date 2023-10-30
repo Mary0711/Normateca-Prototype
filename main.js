81f9a9ff2cd3b5b6f5ecffdd3bdf3820bd94c65f
Jean029 <jean.serrano7@upr.edu>
2023-10-29 23:17:52 -0400

    javascript para los tabs

---
+++ main.js
@@ -0,0 +1,38 @@
+function changeVisibility(tab, lastTab) {
+  lastTab.style.visibility = "hidden";
+  tab.style.visibility = "visible";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = {
+    tabs: {
+      subir: document.getElementById("subir"),
+      editar: document.getElementById("editar"),
+      crear: document.getElementById("crear"),
+    },
+    buttons: {
+      subir: document.getElementById("subirBtn"),
+      editar: document.getElementById("editarBtn"),
+      crear: document.getElementById("crearBtn"),
+    },
+    lastTab: document.getElementById("subir"),
+  };
+
+  elements.buttons.subir.addEventListener("click", () => {
+    changeVisibility(elements.tabs.subir, elements.lastTab);
+
+    elements.lastTab = elements.tabs.subir;
+  });
+
+  elements.buttons.editar.addEventListener("click", () => {
+    changeVisibility(elements.tabs.editar, elements.lastTab);
+
+    elements.lastTab = elements.tabs.editar;
+  });
+
+  elements.buttons.crear.addEventListener("click", () => {
+    changeVisibility(elements.tabs.crear, elements.lastTab);
+
+    elements.lastTab = elements.tabs.crear;
+  });
+});
